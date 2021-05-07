75aec36ed74ca57ff644523714ceb11031aac872
APilganchuk <opilganchuk@gmail.com>
2021-05-07 13:52:47 +0300

    adds marcup to index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import menuItemHbs from './templates/menu-item.hbs';
+import menuData from './menu.json';
 import './styles.css';
+import './localsorage';
+
+const menuContainer = document.querySelector('.js-menu');
+const menuMarcup = createMenuMarcup(menuData);
+
+menuContainer.insertAdjacentHTML('beforeend', menuMarcup);
+
+function createMenuMarcup(menuData) {
+    return menuItemHbs(menuData);
+}
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
